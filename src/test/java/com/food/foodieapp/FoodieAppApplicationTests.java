adc95cd8d33285d4a46de51f9f94e1a0ca2fbec7
Daybel Samuel Diaz Yndalecio <daybel.diaz@ikeasi.com>
2025-06-04 14:12:10 -0400

    Fist Commit

---
+++ src/test/java/com/food/foodieapp/FoodieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.foodieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
